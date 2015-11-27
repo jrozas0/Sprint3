c21d238dc8c402cfd47f3cf63cc014e3682cbe54
Sergio <sergio@noexiste.es>
2015-11-27 19:14:59 +0100

    persistence

---
+++ src/controller/TeacherHelper.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TeacherHelper implements RequestHandler{
+
+	@Override
+	public String handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		return null;
+	}
+}
