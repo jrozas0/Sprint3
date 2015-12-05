924f736c16e0570e0c09681c2f5cea2fd99df751
Eduardo Goncalves <efcbg@iscte.pt>
2015-12-05 21:30:01 +0100

    fixed some controllers

--- MVCLib/src/lib/controllers/ex/MVCException.java
+++
@@ -1,9 +0,0 @@
-package lib.controllers.ex;
-
-public abstract class MVCException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-	
-	public abstract Error getError();
-
-}
